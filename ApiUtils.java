12a300c0c8256038ae3e4acd66cc6d5374b79688
theselol <getbonkd@gmail.com>
2017-03-02 19:49:11 +0100

    Delete ApiUtils.java

--- ApiUtils.java
+++
@@ -1,9 +0,0 @@
-package com.example.android.justspell.data.remote;
-
-public class ApiUtils {
-    public static final String BASE_URL = "http://www.dnd5eapi.co/";
-
-    public static APIService getAPIService(){
-        return RetrofitClient.getClient(BASE_URL).create(APIService.class);
-    }
-}
